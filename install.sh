4ad3b5b6d2ca24d8b1524bd5491df40e49fb0ea0
Zivosh <58852958+Zivosh@users.noreply.github.com>
2021-04-18 19:32:09 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,26 @@
+clear
+
+echo "======================================="
+echo "=======Installing requirements ========"
+echo "======================================="
+
+read -r -p "This script will install all the necessary modules etc on your Pi so you can run the bot"
+
+sudo apt-get update
+
+sudo apt--get upgrade -Y
+
+pip3 install --upgrade pip
+
+pip3 install -U urllib3
+pip3 install -U pandas
+pip3 install -U requests
+pip3 install -U random
+pip3 install -U statsmodel
+pip3 install -U matplotlib
+pip3 install -U binance
+pip3 install -U python-binance
+pip3 install -U cbpro
+pip3 install -U numpy
+
+exit 0
