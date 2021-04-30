70a9ef7a6f073c6772ece16d9a5bdd06ad7c5739
Zivosh <58852958+Zivosh@users.noreply.github.com>
2021-04-30 19:08:10 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ read -r -p "This script will install all the necessary modules etc on your Pi so
 
 sudo apt-get update
 
-sudo apt--get upgrade -Y
+sudo apt-get upgrade -Y
 
 pip3 install --upgrade pip
 
